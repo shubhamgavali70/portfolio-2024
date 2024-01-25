d7cd32f1cc3fc719cffc5a16335a24112c34e142
shubhamgavali70 <shubhamgavali70@gmail.com>
2024-01-25 22:29:21 +0530

    add metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,12 @@
+import type { Metadata } from 'next'
+
 export { default } from '@/screens/HomePage'
+
+export const metadata: Metadata = {
+    metadataBase: new URL('https://acme.com'),
+    title: "It's me 😬",
+    description: "Here you find everything about me..",
+    openGraph: {
+        images: './victory-hand-emoji.png'
+    }
+}
